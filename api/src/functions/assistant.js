8b84c8da2d4016acf9517109a2f4020d8f488515
Wassim Chegham <github@wassim.dev>
2024-05-16 17:20:08 +0200

    chore: use func context.log for logs

--- api/src/functions/assistant.js
+++ api/src/functions/assistant.js
@@ -16,13 +16,13 @@ const {
 // Important: Errors handlings are removed intentionally. If you are using this sample in production
 // please add proper error handling.
 
-async function initAzureOpenAI() {
-    console.log("Using Azure OpenAI (w/ Microsoft Entra ID) ...");
-    const credential = new DefaultAzureCredential();
-    const azureADTokenProvider = getBearerTokenProvider(credential, "https://cognitiveservices.azure.com/.default");
-    return new AzureOpenAI({
-      azureADTokenProvider,
-    });
+async function initAzureOpenAI(context) {
+  context.log("Using Azure OpenAI (w/ Microsoft Entra ID) ...");
+  const credential = new DefaultAzureCredential();
+  const azureADTokenProvider = getBearerTokenProvider(credential, "https://cognitiveservices.azure.com/.default");
+  return new AzureOpenAI({
+    azureADTokenProvider,
+  });
 }
 
 const assistantDefinition = {
@@ -52,49 +52,53 @@ const assistantDefinition = {
   model: AZURE_DEPLOYMENT_NAME,
 };
 
-async function* processQuery(userQuery) {
-  // Step 0: Connect and acquire an OpenAI instance
-  const openai = await initAzureOpenAI();
+async function* processQuery(userQuery, context) {
+  context.log('Step 0: Connect and acquire an OpenAI instance');
+  const openai = await initAzureOpenAI(context);
 
-  // Step 1: Retrieve or Create an Assistant
+  context.log('Step 1: Retrieve or Create an Assistant');
   const assistant = ASSISTANT_ID
     ? await openai.beta.assistants.retrieve(ASSISTANT_ID)
     : await openai.beta.assistants.create(assistantDefinition);
 
-  // Step 2: Create a Thread
+  context.log('Step 2: Create a Thread');
   const thread = await openai.beta.threads.create();
 
-  // Step 3: Add a Message to the Thread
+  context.log('Step 3: Add a Message to the Thread');
   const message = await openai.beta.threads.messages.create(thread.id, {
     role: "user",
     content: userQuery,
   });
 
-  // Step 4: Create a Run (and stream the response)
+  context.log('Step 4: Create a Run (and stream the response)');
   const run = openai.beta.threads.runs.stream(thread.id, {
     assistant_id: assistant.id,
     stream: true,
   });
 
+  context.log('Step 5: Read streamed response', { run });
   for await (const chunk of run) {
     const { event, data } = chunk;
-    console.log({ event, data });
 
     if (event === "thread.message.delta") {
       const delta = data.delta;
 
       if (delta) {
-        const content = delta.content[0]?.text?.value || "";
-        yield content;
+        const value = delta.content[0]?.text?.value || "";
+        yield value;
+        context.log('Processed thread.message.delta', { value });
       }
     } else if (event === "thread.run.requires_action") {
-      yield* handleRequiresAction(openai, data, data.id, data.thread_id);
+      yield* handleRequiresAction(openai, data, data.id, data.thread_id, context);
     }
     // else if ... handle the other events as needed
   }
+
+  context.log('Done!');
 }
 
-async function* handleRequiresAction(openai, run, runId, threadId) {
+async function* handleRequiresAction(openai, run, runId, threadId, context) {
+  context.log('Handle Function Calling');
   try {
     const toolOutputs = await Promise.all(
       run.required_action.submit_tool_outputs.tool_calls.map(
@@ -103,7 +107,8 @@ async function* handleRequiresAction(openai, run, runId, threadId) {
             return {
               tool_call_id: toolCall.id,
               output: await getStockPrice(
-                JSON.parse(toolCall.function.arguments).symbol
+                JSON.parse(toolCall.function.arguments).symbol,
+                context
               ),
             };
           }
@@ -113,15 +118,16 @@ async function* handleRequiresAction(openai, run, runId, threadId) {
     );
 
     // Submit all the tool outputs at the same time
-    yield* submitToolOutputs(openai, toolOutputs, runId, threadId);
+    yield* submitToolOutputs(openai, toolOutputs, runId, threadId, context);
   } catch (error) {
-    console.error("Error processing required action:", error);
+    context.log.error("Error processing required action:", error);
   }
 }
 
-async function* submitToolOutputs(openai, toolOutputs, runId, threadId) {
+async function* submitToolOutputs(openai, toolOutputs, runId, threadId, context) {
   try {
     // Use the submitToolOutputsStream helper
+    context.log('Call Tool output and stream the response');
     const asyncStream = openai.beta.threads.runs.submitToolOutputsStream(
       threadId,
       runId,
@@ -130,22 +136,23 @@ async function* submitToolOutputs(openai, toolOutputs, runId, threadId) {
     for await (const chunk of asyncStream) {
       if (chunk.event === "thread.message.delta") {
         // stream message back to UI
-        const delta = chunk.data.delta;
+        const { delta } = chunk.data;
 
         if (delta) {
-          const content = delta.content[0]?.text?.value || "";
-          yield content;
+          const value = delta.content[0]?.text?.value || "";
+          yield value;
+          context.log('Processed thread.message.delta (tool output)', { value });
         }
       }
       // else if ... handle the other events as needed
     }
   } catch (error) {
-    console.error("Error submitting tool outputs:", error);
+    context.log.error("Error submitting tool outputs:", error);
   }
 }
 
-async function getStockPrice(symbol) {
-  console.log("fetching fake closing stock value for symbol:".symbol);
+async function getStockPrice(symbol, context) {
+  context.log("Fetching fake closing stock value for symbol:", { symbol });
   return Promise.resolve("" + Math.random(10) * 1000); // simulate network request
 }
 
@@ -158,6 +165,11 @@ app.http("assistant", {
     context.log(`Http function processed request for url "${request.url}"`);
     const query = await request.text();
 
-    return { body: Readable.from(processQuery(query)) };
+    return {
+      headers: {
+        'Content-Type': 'text/plain',
+        "Transfer-Encoding": "chunked"
+      }, body: Readable.from(processQuery(query, context))
+    };
   },
 });
